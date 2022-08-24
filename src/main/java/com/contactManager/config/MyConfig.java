ae0ee5b2f8e964e460e5fe43ef82f84b36d73b3e
Sumitk09 <sumit.k.09@gmail.com>
2022-08-24 17:53:24 +0530

    First Commit
    first Github project

---
+++ src/main/java/com/contactManager/config/MyConfig.java
@@ -0,0 +1,9 @@
+package com.contactManager.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfig {
+
+	
+}
---
+++ src/main/java/com/contactManager/controller/HomeController.java
@@ -0,0 +1,81 @@
+package com.contactManager.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.aspectj.bridge.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.contactManager.dao.UserRepository;
+import com.contactManager.entities.User;
+
+@Controller
+public class HomeController {
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("title","Home - Smart Contact Manager");
+		return "home";
+	}
+	
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title","About - Smart Contact Manager");
+		return "about";
+	}
+	
+	@RequestMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("title","Register - Smart Contact Manager");
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+	
+	//handler for registering user
+	@RequestMapping(value = "/do_register", method = RequestMethod.POST)
+	public String registerUser(@ModelAttribute("user") User user, @RequestParam(value = "agreement",defaultValue = "false") boolean agreement, Model model, HttpSession session) {
+		
+		try {
+			
+			if(!agreement) {
+				
+				System.out.println("You have not agreed the terms and conditions");
+				throw new Exception("You have not agreed the terms and conditions");
+			}
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+			user.setImageUrl("default.png");
+			
+			System.out.println("Agreement "+ agreement);
+			System.out.println("USER "+ user);
+				
+			User result = this.userRepository.save(user);
+				
+			model.addAttribute("user", new User());
+			
+			session.setAttribute("message", new com.contactManager.helper.Message("Sucessfully Registered !!", "alert-sucess"));
+			return "signup";
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new com.contactManager.helper.Message("Something went wrong !!" + e.getMessage(), "alert-danger"));
+			return "signup";
+		}
+		
+		
+		
+		
+	}
+}
